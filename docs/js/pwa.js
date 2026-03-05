bfcec0ab8b27ee6fc7ef098843f0df90753d8b29
Jose Rico Harvey Saberon <jrsaberon@ccc.edu.ph>
2026-03-05 19:11:43 +0800

    Add PWA setup with manifest, service worker, and app icons

---
+++ docs/js/pwa.js
@@ -0,0 +1,13 @@
+(function () {
+  if (!("serviceWorker" in navigator)) {
+    return;
+  }
+
+  window.addEventListener("load", function () {
+    var swPath = location.pathname.includes("/html/") ? "../sw.js" : "./sw.js";
+
+    navigator.serviceWorker.register(swPath).catch(function (error) {
+      console.warn("Service worker registration failed:", error);
+    });
+  });
+})();
